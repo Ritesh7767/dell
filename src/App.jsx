45e953099014644eed407cafed47d3a67de0c42c
Ritesh7767 <ritesh776782@gmail.com>
2024-06-08 19:03:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import AllNavbar from "./Pages/NavPage";
+import AllBody from "./Pages/AllBody";
+import Home from "./Pages/Home";
+import Footer from "./Pages/FooterPage";
+import AllRoutes from "./Pages/AllRoutes";
+
+
+
+export default function App() {
+  return (
+    <div >
+       <AllNavbar />
+       <AllRoutes/>
+       {/* <AllBody />  */}
+      {/* <Home /> */}
+       <Footer /> 
+    </div>
+  );
+}
